ad6e4de848c561e89859e8f6ffb08d514326f8b1
ShaylaJan <shayla.jansen@amsterdam.nl>
2023-07-14 11:52:07 +0000

    fix: Fix filename input

--- notebooks/visualize_poles.py
+++ notebooks/visualize_poles.py
@@ -6,17 +6,26 @@ import laspy
 from upc_analysis import visualization
 import multiprocessing
 
+# Select location
+#my_location = '201001' # Oost
+#my_location = '200923' # Zuidoost
+my_location = '200921' # Centrum
+#my_location = '200920' # Nieuw-West
+#my_location = '200918' # Noord
+#my_location = '200903' # Zuid
+#my_location = '200824' # West
+#my_location = '200823' # Haven
 
 # Paths - point cloud data
 base_folder = '/home/azureuser/cloudfiles/code/blobfuse/ovl'
-dataset_folder = f'{base_folder}/pointcloud/Unlabeled/Amsterdam/nl-amsd-200923-7415-laz/las_processor_bundled_out/'  # point clouds
-pred_folder = f'{base_folder}/predictions/nl-amsd-200923-7415-laz/'  # predictions as npz files
+dataset_folder = f'{base_folder}/pointcloud/Unlabeled/Amsterdam/nl-amsd-{my_location}-7415-laz/las_processor_bundled_out/'  # point clouds
+pred_folder = f'{base_folder}/predictions/nl-amsd-{my_location}-7415-laz/'  # predictions as npz files
 prefix = 'filtered_'
 prefix_pred = 'pred_'
 
 # Paths - extracted poles
-output_file_filter = base_folder + "/20230707-191830_poles_extracted_zuidoost_filtered.csv"
-img_out_folder = f'{base_folder}/images/20230707-191830/'
+output_file_filter = f'{base_folder}/poles_extracted_{my_location}_filtered.csv'
+img_out_folder = f'{base_folder}/images/{my_location}/'
 
 # Load poles data
 poles_df = pd.read_csv(output_file_filter)
