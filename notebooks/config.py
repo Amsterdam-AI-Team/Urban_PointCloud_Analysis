da04a1cf4233577251e6ed02ad5dd5bbca9ef4b4
ShaylaJan <shayla.jansen@amsterdam.nl>
2023-08-04 11:46:26 +0000

    feature: create config file setup

---
+++ notebooks/config.py
@@ -0,0 +1,34 @@
+# Input paths
+base_folder = '../datasets'
+
+dataset_folder = f'{base_folder}/pointcloud/'  # folder with point clouds
+pred_folder = f'{base_folder}/predictions/'  # folder with predictions as npz files
+prefix = 'filtered_'
+prefix_pred = 'pred_'
+
+ahn_data_folder = f'{base_folder}/ahn/'
+bgt_building_file = f'{base_folder}/bgt/bgt_buildings.csv'
+trees_file = f'{base_folder}/trees/obj_vgo_boom_view.gpkg'
+
+# Output paths
+output_file = f'{base_folder}/poles_extracted.csv'
+output_file_filter = f'{base_folder}/poles_extracted_filtered.csv'
+img_out_folder = f'{base_folder}/images/'
+
+# Define the class we are interested in
+target_label = 60
+
+# Labels used for ground points ('Road' and 'Other ground')
+ground_labels = [1, 9]
+
+# Settings for noise filtering
+EPS_N = 0.1
+MIN_SAMPLES_N = 50
+
+# Settings for clustering
+EPS = 0.1
+MIN_SAMPLES = 150
+
+# Filter settings for minimum and maximum height (m)
+MIN_HEIGHT = 1.8
+MAX_HEIGHT = 16
--- notebooks/visualize_poles.py
+++ notebooks/visualize_poles.py
@@ -4,21 +4,11 @@ import numpy as np
 import laspy
 from upc_analysis import visualization
 import multiprocessing
-
-# Paths - point cloud data
-base_folder = '../datasets'
-dataset_folder = f'{base_folder}/pointcloud/'  # folder with point clouds
-pred_folder = f'{base_folder}/predictions/'  # folder with predictions as npz files
-prefix = 'filtered_'
-prefix_pred = 'pred_'
-
-# Paths - extracted poles
-output_file_filter = f'{base_folder}/poles_extracted_filtered.csv'
-img_out_folder = f'{base_folder}/images/'
+import config as cf
 
 # Load poles data
-poles_df = pd.read_csv(output_file_filter)
+poles_df = pd.read_csv(cf.output_file_filter)
 poles_df.sort_values('tilecode', inplace=True)
 
 # Save png of object x, y and 3d axis
-visualization.create_images_for_poles(poles_df, dataset_folder, pred_folder, img_out_folder, prefix, prefix_pred)
+visualization.create_images_for_poles(poles_df, cf.dataset_folder, cf.pred_folder, cf.img_out_folder, cf.prefix, cf.prefix_pred)
